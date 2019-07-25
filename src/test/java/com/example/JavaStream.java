5c6f6413590e24e3133880ab8d14cbe839526ce8
wuxii <wuxii@foxmail.com>
2019-07-25 23:19:53 +0800

    add log output

--- src/test/java/com/example/JavaStream.java
+++ src/test/java/com/example/JavaStream.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,13 +8,14 @@ import java.util.Arrays;
 /**
  * @author wuxii
  */
+@Log4j2
 public class JavaStream {
 
     @Test
     public void streamTest() {
         Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
                 .parallelStream()
-                .forEach(System.out::print);
+                .forEach(log::info);
     }
 
 }
